5ffbad4556c85f34539b4a494adfa80d13d0f420
Christian Volz <christianbvolz@gmail.com>
2021-09-20 09:08:38 -0300

    requisito 5

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -2,11 +2,41 @@ import React from 'react';
 import Header from '../components/Header';
 
 class Search extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      search: '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange({ target: { value } }) {
+    this.setState({ search: value });
+  }
+
   render() {
+    const { search } = this.state;
+    const minimumCharacter = 2;
     return (
       <div data-testid="page-search">
         <Header />
-        Search
+        <form>
+          <label htmlFor="input-search">
+            <input
+              type="text"
+              data-testid="search-artist-input"
+              onChange={ this.handleChange }
+            />
+          </label>
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ search.length < minimumCharacter }
+            // onClick={  }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
