429834bd7eea06d718acef59b3d35452b84ed9a3
Christian Volz <christianbvolz@gmail.com>
2021-09-20 08:53:58 -0300

    requisito 4

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Loading from './Loading';
 import { getUser } from '../services/userAPI';
 
@@ -38,6 +39,11 @@ class Header extends React.Component {
     return (
       <header data-testid="header-component">
         {(loading) ? <Loading /> : this.renderUser()}
+        <nav>
+          <Link to="/search" data-testid="link-to-search">Search</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+          <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+        </nav>
       </header>
     );
   }
